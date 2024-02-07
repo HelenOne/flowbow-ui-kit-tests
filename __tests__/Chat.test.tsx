0c9797e5b795484a770df20ba04e2b17895fe355
Elena Gromova <elena.gromova@hexlet.io>
2024-02-07 18:34:16 +0300

    tests

---
+++ __tests__/Chat.test.tsx
@@ -0,0 +1,33 @@
+// Предположим, что ваш файл с тестами называется Chat.test.tsx
+
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';  // Для расширенных матчеров Jest
+
+import Chat from '../src/Components/Chat';
+import steps from '../__fixtures__/steps';
+import { Step } from '../src/interfaces/Step';
+
+import { test, expect } from '@jest/globals'
+
+import '@testing-library/jest-dom';
+
+test('Init state', () => {
+  render(<Chat steps={steps as Step[]} />);
+  expect(screen.getByText(/Привет! Я ваш виртуальный помощник/i)).toBeInTheDocument();
+});
+
+test('Update steps', () => {
+  render(<Chat steps={steps as Step[]} />);
+  
+  const button = screen.getByText(/Интересно/i);
+  
+  fireEvent.click(button);
+  
+  expect(screen.getByText(/У нас есть подготовительные курсы/i)).toBeInTheDocument();
+  
+  expect(screen.getByText(/Интересно/i)).toBeInTheDocument();
+});
+
+// Другие тесты...
+
---
+++ __tests__/WidgetModal.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';  // Для расширенных матчеров Jest
+
+import WidgetModal from '../src/Components/WidgetModal';
+import { Step } from '../src/interfaces/Step';
+
+import steps from '../__fixtures__/steps';
+
+import { test, expect } from '@jest/globals';
+
+import '@testing-library/jest-dom';
+
+
+test('отображение и закрытие модального окна', () => {
+  render(<WidgetModal steps={steps as Step[]} />);
+  
+  // Проверяем, что кнопка "Открыть Чат" присутствует на экране
+  const openButton = screen.getByText(/Открыть Чат/i);
+  expect(openButton).toBeInTheDocument();
+  
+  // Имитируем клик по кнопке открытия чата
+  fireEvent.click(openButton);
+  
+  // Проверяем, что модальное окно появляется
+  expect(screen.getByText(/Виртуальный помощник/i)).toBeInTheDocument();
+  
+  // Имитируем клик по кнопке закрытия модального окна
+  const closeButton = screen.getByLabelText(/закрыть/i);
+  fireEvent.click(closeButton);
+  
+  // Проверяем, что модальное окно закрывается
+  expect(screen.queryByText(/Виртуальный помощник/i)).not.toBeInTheDocument();
+});
+
+// Другие тесты...
